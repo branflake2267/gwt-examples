a4cfbcd24e9bda16f54ec31885d7186f271d691f
branflake2267 <branflake2267@4795bbb2-1543-0410-831c-f9a674249a0d>
2008-01-19 09:07:17 +0000

    updated style

--- gwt-test-Login-Manager/src/com/tribling/gwt/test/loginmanager/client/LoginManager.java
+++ gwt-test-Login-Manager/src/com/tribling/gwt/test/loginmanager/client/LoginManager.java
@@ -1,48 +1,59 @@
 package com.tribling.gwt.test.loginmanager.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.ChangeListener;
-import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
+
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class LoginManager implements EntryPoint {
 
-  /**
-   * This is the entry point method.
-   */
-  public void onModuleLoad() {
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
 
-	  //check session cookie first
-	  LoginManagerSessionCheck sc = new LoginManagerSessionCheck();
-	  
-	  
-	  
-	  
-	  //display the widget
-	  LoginPanelWidget lpw = new LoginPanelWidget();
-	  lpw.addChangeListener(new ChangeListener() {
+		//debugging
+		RootPanel.get().add(new Label("My URL-Path: /GoneVerticalService"));
+
+		RootPanel.get().add(new Label("GWT getModuleBaseURL: " + GWT.getModuleBaseURL()));
+
+		RootPanel.get().add(new Label("GWT getHostPageBaseURL: " + GWT.getHostPageBaseURL()));
+
+		RootPanel.get().add(new Label("GWT getModuleBaseURL: " + GWT.getModuleBaseURL()));
+		
+		RootPanel.get().add(new Label("GWT getModuleName: " + GWT.getModuleName()));
+
+		//check session cookie first
+		final SessionManagerWidget sm = new SessionManagerWidget();
+		sm.addChangeListener(new ChangeListener() {
 
 			public void onChange(Widget sender) {
 
-				//do anything u want here?
-				//Window.alert("Sign In Selected");
-				
-				//notify another widget, use widgets methods here to do stuff
-				//in widget's onclick method set a variable/object
-				//then use widgets method right here to get the variable/object
-				
+				if (sm.getLoginStatus() == true) {
+					loadPanel(sm.getSessionID());
+				}
+
+				if (sm.getLoginStatus() == false) {
+					//unloadWidgets();
+				}
+
 			}
 		});
 
-	  
-	  RootPanel.get().add(lpw);
-	  
-  }
+	}
+	
+	
+	public void loadPanel(String SessionID) {
+		
+		RootPanel.get().add(new Label("Logged In: SessionID" + SessionID));
+		
+	}
+	
+	
 }
