21bf7a86ba482cf7d30117dd726dadb4b336c4fe
branflake2267 <branflake2267@4795bbb2-1543-0410-831c-f9a674249a0d>
2008-01-23 22:06:53 +0000

    errors are happening, transfering to another computer to see if that happens

--- gwt-test-Login-Manager/src/com/tribling/gwt/test/loginmanager/client/AccountWidget2.java
+++ gwt-test-Login-Manager/src/com/tribling/gwt/test/loginmanager/client/AccountWidget2.java
@@ -4,47 +4,29 @@ package com.tribling.gwt.test.loginmanager.client;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.IncompatibleRemoteServiceException;
+import com.google.gwt.user.client.rpc.InvocationException;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
 import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.PushButton;
-import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class AccountWidget2 extends Composite implements ClickListener{
 
-	
-	//GET TIMESTAMP
+
 	
 	
 	
 	//rpc init Var 
 	private LoginManagerServiceAsync callProvider;
 	
-	private VerticalPanel NewAccountPanel = new VerticalPanel();
-
-	private Label TitleNew = new Label("Make A New Account");
-	private Label TitleEdit = new Label("Edit Your Account");
-	
-	private HorizontalPanel pDisplayError = new HorizontalPanel();
-	
-	private HorizontalPanel pLoadingStatus = new HorizontalPanel();
-	
-	//Inputs
-	private TextBox FirstName = new TextBox();
-	private TextBox LastName = new TextBox();
-	private TextBox UserName = new TextBox();
-	private PasswordTextBox Password1 = new PasswordTextBox();
-	private PasswordTextBox Password2 = new PasswordTextBox();
 	
 	private PushButton Save = new PushButton("Save");
-		
-	private String SessionID;
-	
 
 	
 	
@@ -53,77 +35,16 @@ public class AccountWidget2 extends Composite implements ClickListener{
 	 */
 	public AccountWidget2() {
 		
-		this.SessionID = SessionID;
-		
-		HorizontalPanel hpFN = new HorizontalPanel();
-		Label lFN = new Label("First Name:");
-		lFN.setStyleName("account-Fields");
-		hpFN.add(lFN);
-		hpFN.add(FirstName);
-	
-		HorizontalPanel hpLN = new HorizontalPanel();
-		Label lLN = new Label("Last Name:");
-		lLN.setStyleName("account-Fields");
-		hpLN.add(lLN);
-		hpLN.add(LastName);
-		
-		HorizontalPanel hpUN = new HorizontalPanel();
-		Label lUserName = new Label("User Name");
-		lUserName.setStyleName("account-Fields");
-		hpUN.add(lUserName);
-		hpUN.add(UserName);
-		
-		
-		HorizontalPanel hpP1 = new HorizontalPanel();
-		Label lP1 = new Label("Password:");
-		lP1.setStyleName("account-Fields");
-		hpP1.add(lP1);
-		hpP1.add(Password1);
-		
-		HorizontalPanel hpP2 = new HorizontalPanel();
-		Label lP2 = new Label("Confirm:");
-		lP2.setStyleName("account-Fields");
-		hpP2.add(lP2);
-		hpP2.add(Password2);
-		
-		HorizontalPanel hpSave = new HorizontalPanel();
-		hpSave.setHorizontalAlignment(HorizontalPanel.ALIGN_RIGHT);
-		hpSave.add(Save);
-		hpSave.add(pLoadingStatus);
-		
-		
-		//New User or Editing Account via SessionID
-		if (SessionID != null) {
-			NewAccountPanel.add(TitleNew);
-		} else {
-			NewAccountPanel.add(TitleEdit);
-		}
-		
-		
-		NewAccountPanel.add(pDisplayError);
-		NewAccountPanel.add(hpFN);
-		NewAccountPanel.add(hpLN);
-		NewAccountPanel.add(hpUN);
-		NewAccountPanel.add(hpP1);
-		NewAccountPanel.add(hpP2);
-		NewAccountPanel.add(hpSave);
-		
-		//init the Account Widget
-		initWidget(NewAccountPanel);
+		VerticalPanel vp = new VerticalPanel();
+		vp.add(Save);
+		initWidget(Save);
 		
 		Save.addClickListener(this);
 		
-	}
-
-	/**
-	 * prep for transport of data
-	 */
-	private void saveData() {
-		
-		//init rpc request - send the data
 		SaveAccount();
 	}
-	
+
+
 
 	
 
@@ -136,10 +57,8 @@ public class AccountWidget2 extends Composite implements ClickListener{
 	 */
 	public void onClick(Widget sender) {
 		
-		if (sender == Save) {
-			this.saveData();
-		}
-
+		SaveAccount();
+		
 	}
 
 	
@@ -166,10 +85,9 @@ public class AccountWidget2 extends Composite implements ClickListener{
 	 * 
 	 */
     public void SaveAccount() {
-    	//this.drawLoading();
     	
 		// service returns a result
-		AsyncCallback callback_SaveAccount = new AsyncCallback() {
+		AsyncCallback callback = new AsyncCallback() {
 			
 			//ajax rpc fail
 			public void onFailure(Throwable caught) {
@@ -177,17 +95,14 @@ public class AccountWidget2 extends Composite implements ClickListener{
 
 			//ajax rpc success
 			public void onSuccess(Object result) {
+				//SignInStatus sis = (SignInStatus) result; //cast the result into the object to use
+				//processCallBack(sis);
 			}
 		};
 
 		// execute the service and request for rpc method
-		try {
-			Account account = new Account();
-			account.setSessionID("12");
-			callProvider.saveAccount(account, callback_SaveAccount);
-		} catch (RuntimeException e) {
-			e.printStackTrace();
-		}
+		callProvider.processSignIn("test", "test2", callback);
+		
     }
 
     
