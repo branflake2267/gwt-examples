d7a7eec2fa77b7f5ee3d707c6c7bac319b5a92cb
branflake2267@gmail.com <branflake2267@gmail.com@4795bbb2-1543-0410-831c-f9a674249a0d>
2008-08-29 02:45:51 +0000

    Uploaded the latest to gawkat.

--- trunk/gwt-Calendar/src/com/tribling/gwt/test/calendar/client/CalendarData.java
+++ trunk/gwt-Calendar/src/com/tribling/gwt/test/calendar/client/CalendarData.java
@@ -4,6 +4,12 @@ package com.tribling.gwt.test.calendar.client;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
+/**
+ * I used this to put labels on the calendar. 
+ * I use this to transport the calendar labels around, like from an rpc call
+ * @author branflake2267
+ *
+ */
 public class CalendarData implements IsSerializable {
 
 	public int day = 0;
@@ -11,8 +17,6 @@ public class CalendarData implements IsSerializable {
 	public int year = 0;
 	public String label = null;
 	
-	public int desiredResponse = 0; //0,1=yes, 2=no, for main todo, expecting yes or no 
-	public String yesno = null;
 	
 	/**
 	 * constructor - empty
