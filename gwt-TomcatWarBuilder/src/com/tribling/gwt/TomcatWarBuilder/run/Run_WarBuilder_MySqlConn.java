1e633067e13599f8acfbdf8d8dbc79aeb8aaa64b
branflake2267 <branflake2267@gmail.com>
2008-10-26 04:02:29 +0000

    made another example run

--- gwt-TomcatWarBuilder/src/com/tribling/gwt/TomcatWarBuilder/run/Run_WarBuilder_MySqlConn.java
+++ gwt-TomcatWarBuilder/src/com/tribling/gwt/TomcatWarBuilder/run/Run_WarBuilder_MySqlConn.java
@@ -1,5 +1,52 @@
 package com.tribling.gwt.TomcatWarBuilder.run;
 
-public class Run_WarBuilder_MySqlConn {
+import com.tribling.gwt.TomcatWarBuilder.TomcatWarBuilder;
+import com.tribling.gwt.TomcatWarBuilder.WarCompileData;
 
+public class Run_WarBuilder_MySqlConn {
+	
+	/**
+	 * run this from console or debug configuration in eclipse
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// refer to this object for variable options
+		WarCompileData data = new WarCompileData();
+		
+		// choose your operating system
+		// [0=linux, 1=windows]		
+		
+		data.os = 0;
+		
+		// project directory (examples below)
+		data.projectDirectory = "/home/branflake2267/workspace/gwt-test-MySQLConn";
+
+		// rename war file to [ROOT.war | project.war]
+		// ROOT.war is the virtual host root servlet context "/" 
+		data.renameWarFile = false; //true = turn on will rename the servlet context (filename)
+		
+		// if renameWarFile = true then rename to:
+		data.renameWarFileNameTo = "ROOT.war"; 
+		
+
+		/********** OPTIONAL ***********/
+		
+		// go to application instead of going to index.jsp true is recommended
+		data.goDirectlyToApplication = true;
+		
+		// Ask for credentials to use application 
+		data.askForLogin = false;
+		
+		data.deleteTempFolder = true;
+		
+		/********** OPTIONAL ***********/
+		
+		
+		// set up the object that will build the war 
+		TomcatWarBuilder build = new TomcatWarBuilder();
+		build.setTomcatCompileData(data);
+		build.start();
+	}
 }
