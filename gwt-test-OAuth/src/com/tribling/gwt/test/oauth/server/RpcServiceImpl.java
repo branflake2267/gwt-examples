656a034cb5453fb4d0165029e393c5fa2fc71b1b
branflake2267 <branflake2267@gmail.com>
2008-11-24 04:44:26 +0000

    Started the access request

--- gwt-test-OAuth/src/com/tribling/gwt/test/oauth/server/RpcServiceImpl.java
+++ gwt-test-OAuth/src/com/tribling/gwt/test/oauth/server/RpcServiceImpl.java
@@ -1,5 +1,12 @@
 package com.tribling.gwt.test.oauth.server;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpUtils;
+
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.user.client.HTTPRequest;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.tribling.gwt.test.oauth.client.oauth.OAuthTokenData;
 import com.tribling.gwt.test.oauth.client.rpc.RpcService;
@@ -23,8 +30,12 @@ public class RpcServiceImpl extends RemoteServiceServlet implements RpcService {
 	 * A. ->(B.?) grant request token?
 	 */
 	public OAuthTokenData requestToken(OAuthTokenData tokenData) {
+		
+		HttpServletRequest request = getThreadLocalRequest();
+		String url = request.getRequestURL().toString();
+		
 		OAuthServer oauth = new OAuthServer();
-		return oauth.requestToken(tokenData);
+		return oauth.requestToken(tokenData, url);
 	}
 
 	
