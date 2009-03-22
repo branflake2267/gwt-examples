822b8512f09622124210f2427edac570088d868e
branflake2267@gmail.com <branflake2267@gmail.com@4795bbb2-1543-0410-831c-f9a674249a0d>
2009-03-22 20:26:28 +0000

    oauth moved to core system functions, as a user management system... setting up in 1.6.2

---
+++ trunk/System/src/com/gawkat/gwt/system/client/rpc/RpcService.java
@@ -0,0 +1,34 @@
+package com.gawkat.gwt.system.client.rpc;
+
+import com.gawkat.gwt.system.client.account.UserData;
+import com.gawkat.gwt.system.client.oauth.OAuthTokenData;
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+@RemoteServiceRelativePath("rpcSystem")
+public interface RpcService extends RemoteService {
+
+	/**
+	 * example rpc request
+	 * 
+	 * @param s
+	 * @return
+	 */
+	public String testMethod(String s);
+
+	/**
+	 * A. grant request token?
+	 * 
+	 * @param tokenData
+	 * @return
+	 */
+	public OAuthTokenData requestToken(OAuthTokenData tokenData);
+	
+  public UserData createUser(UserData userData);
+  
+  public UserData isUserNameExist(UserData userData);
+  
+  public UserData forgotPassword(UserData userData);
+  
+  public OAuthTokenData getUserAccessToken(OAuthTokenData appAccessToken);
+}
