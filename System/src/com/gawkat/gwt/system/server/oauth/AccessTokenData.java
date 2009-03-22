93fa2e50dc653bbff602725e564186e3e34851f1
branflake2267 <branflake2267@gmail.com>
2009-03-22 20:26:28 +0000

    oauth moved to core system functions, as a user management system... setting up in 1.6.2

---
+++ System/src/com/gawkat/gwt/system/server/oauth/AccessTokenData.java
@@ -0,0 +1,11 @@
+package com.gawkat.gwt.system.server.oauth;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class AccessTokenData implements IsSerializable {
+
+	public String accessToken = null;
+	
+	public String accessTokenSecret = null;
+	
+}
---
+++ System/src/com/gawkat/gwt/system/server/oauth/ApplicationData.java
@@ -0,0 +1,11 @@
+package com.gawkat.gwt.system.server.oauth;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class ApplicationData implements IsSerializable {
+
+	public int applicationId;
+	public String consumerKey;
+	public String consumerSecret;
+	
+}
