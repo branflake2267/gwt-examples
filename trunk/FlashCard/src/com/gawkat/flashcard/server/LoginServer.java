0772f84f0b3184cb21c8453c69e6fb6f0c0fed4d
branflake2267@gmail.com <branflake2267@gmail.com@4795bbb2-1543-0410-831c-f9a674249a0d>
2009-05-24 05:18:44 +0000

    jdo setup

--- trunk/FlashCard/src/com/gawkat/flashcard/server/LoginServer.java
+++ trunk/FlashCard/src/com/gawkat/flashcard/server/LoginServer.java
@@ -29,4 +29,27 @@ public class LoginServer {
     return loginData; 
   }
   
+  public LoginData getLoginData() {
+  
+    UserService userService = UserServiceFactory.getUserService();
+    User user = userService.getCurrentUser();
+
+    LoginData loginData = new LoginData();
+    if (user != null) {
+      loginData.setLoggedIn(true);
+      loginData.setEmailAddress(user.getEmail());
+      loginData.setNickname(user.getNickname());
+      //loginData.setLogoutUrl(userService.createLogoutURL(requestUri));
+    } else {
+      loginData.setLoggedIn(false);
+      //loginData.setLoginUrl(userService.createLoginURL(requestUri));
+    }
+
+    return loginData; 
+  }
+  
+  public User getUser() {
+    UserService userService = UserServiceFactory.getUserService();
+    return userService.getCurrentUser();
+  }
 }
--- trunk/FlashCard/src/com/gawkat/flashcard/server/MathDataServer.java
+++ trunk/FlashCard/src/com/gawkat/flashcard/server/MathDataServer.java
@@ -1,9 +1,12 @@
 package com.gawkat.flashcard.server;
 
 import com.gawkat.flashcard.client.card.MathData;
+import com.google.appengine.api.users.User;
 
 public class MathDataServer {
 
+  private LoginServer login = new LoginServer();
+  
   public MathDataServer() {
   }
   
@@ -21,10 +24,22 @@ public class MathDataServer {
       mathData = getEasy(mathData);
     }
     
+    // remember the last values the user pulled
+    // this will only save if a user is logged in
+    mathData.save();
+    
     return mathData;
   }
-    
+  
   private MathData getStart() {
+
+    // is the user logged in?
+    User user = null;
+    if (login.getLoginData().isLoggedIn() == true) {
+      user = login.getUser();
+    }
+    
+    // TODO - if the user is logged in, see if there is a previous mathData to use
     
     MathData mathData = new MathData();
     
