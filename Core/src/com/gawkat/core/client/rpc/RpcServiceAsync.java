d878c08c80269f80439932dc663b262deea483c3
branflake2267@gmail.com <branflake2267@gmail.com@4795bbb2-1543-0410-831c-f9a674249a0d>
2009-06-27 15:34:17 +0000

    System project rename

---
+++ Core/src/com/gawkat/core/client/rpc/RpcServiceAsync.java
@@ -0,0 +1,32 @@
+package com.gawkat.core.client.rpc;
+
+import com.gawkat.core.client.account.UserData;
+import com.gawkat.core.client.oauth.OAuthTokenData;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface RpcServiceAsync {
+
+	/**
+	 * example rpc request with the callback
+	 * 
+	 * @param s
+	 * @param callback
+	 */
+	public void testMethod(String s, AsyncCallback<String> callback);
+	
+	/**
+	 * A. grant request token?
+	 * 
+	 * @param tokenData
+	 * @param callback
+	 */
+	public void requestToken(OAuthTokenData tokenData, AsyncCallback<OAuthTokenData> callback);
+	
+  public void createUser(UserData userData, AsyncCallback<UserData> callback);
+  
+  public void isUserNameExist(UserData userData, AsyncCallback<UserData> callback);
+  
+  public void forgotPassword(UserData userData, AsyncCallback<UserData> callback);
+  
+  public void getUserAccessToken(OAuthTokenData appAccessToken, AsyncCallback<OAuthTokenData> callback);
+}
