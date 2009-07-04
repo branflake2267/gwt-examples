e8531c9b1b4b0f5b89df8ca5c1b8e5fe6440fd5d
branflake2267 <branflake2267@gmail.com>
2009-07-04 00:35:51 +0000

    making a backup before project changes

--- FlashCard/src/com/gawkat/flashcard/client/gadget/MyGadgetFeatures.java
+++
@@ -1,9 +0,0 @@
-package com.gawkat.flashcard.client.gadget;
-
-import com.google.gwt.gadgets.client.GadgetFeature.FeatureName;
-
-// TODO - does this have anything to do with the .gadget.xml file artifact request?
-// @ FeatureName(value={""})
-public interface MyGadgetFeatures {
-
-}
--- ver15/gwt-test-RPC-basic/src/com/tribling/gwt/RPC/basic/client/BasicService.java
+++
@@ -1,9 +0,0 @@
-package com.tribling.gwt.RPC.basic.client;
-
-import com.google.gwt.user.client.rpc.RemoteService;
-
-public interface BasicService extends RemoteService {
-	
-	public String myMethod(String s);
-	
-}
