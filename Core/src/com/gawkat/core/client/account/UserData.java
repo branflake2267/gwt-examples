aa1f81e056b049b07c10ef2848dbd37cb4ba4e30
branflake2267 <branflake2267@gmail.com>
2009-07-04 00:40:32 +0000

    change of format for core, moving to standalone, can't get it to mix with a nother project, moving to develop it first on its own

---
+++ Core/src/com/gawkat/core/client/account/UserData.java
@@ -0,0 +1,179 @@
+package com.gawkat.core.client.account;
+
+import com.gawkat.core.client.oauth.OAuthTokenData;
+import com.gawkat.core.client.oauth.Sha1;
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class UserData implements IsSerializable {
+
+  // possible errors for error notifications
+  final public static int SYSTEM_ERROR = 1;
+  final public static int KEY_EXISTS = 2;
+  final public static int KEYS_DONTMATCH = 3;
+  final public static int SECRETS_DONTMATCH = 4;
+  final public static int BOTH_DONTMATCH = 5;
+  final public static int KEYS_SHORT = 6;
+  final public static int KEY1_SHORT = 7;
+  final public static int KEY2_SHORT = 8;
+  final public static int SECRETS_SHORT = 9;
+  final public static int SECRET1_SHORT = 10;
+  final public static int SECRET2_SHORT = 11;
+  final public static int BOTH_SHORT = 12;
+  final public static int SIGNATURE_ERROR = 13;
+  final public static int ACCEPT_TERMS = 14;
+  final public static int ERR_NO_USER = 15;
+  
+  
+  // used on server side
+  public Long userId;
+  
+  // consumer accessToken
+  // web application has access
+  // will be used to verify this application can create users
+  // will be used to verify this application can login users
+  // its like a sessionVars that are for a non-user session
+  public OAuthTokenData accessToken = null; 
+  
+  // userName for login
+  public String consumerKey = null;
+  
+  // hash of password
+  public String consumerSecret = null;
+
+  // accept terms of use
+  public boolean acceptTerms = false;
+  
+  // error notifications
+  public int error = 0;
+  
+
+  
+  // hash of this objects vars
+  // verify it was disturbed during transit
+  private String signature = null;
+  
+  
+  /**
+   * constructor - nothing to do
+   */
+  public UserData() {
+  }
+  
+  /**
+   * sign this object variables that I don't want messed with
+   * TODO - add some salt? - keeping it simple for now, return to later
+   * 
+   */
+  public void sign() {
+    Sha1 sha = new Sha1();
+    this.signature = sha.b64_sha1(getSignatureBaseString());
+  }
+  
+  /**
+   * verify this objects varibles
+   * TODO - add some salt?
+   * 
+   * @return
+   */
+  public boolean verify() {
+    Sha1 sha = new Sha1();
+    
+    boolean pass = false;
+    if (this.signature.equals(sha.b64_sha1(getSignatureBaseString()))) {
+      pass = true;
+    }
+    
+    return pass;
+  }
+  
+  private String getSignatureBaseString() {
+    String s = "";
+    s += "consumerKey=" + consumerKey + "&";
+    s += "consumerSecret=" + consumerSecret;
+    return s;
+  }
+  
+  public String getNotification() {
+    String s = "";
+    switch (error) {
+    case SIGNATURE_ERROR:
+      s = "Signature does not match. Verification of signature problem. Internal error.";
+      break;
+    case UserData.SYSTEM_ERROR:
+      s = "System error occurred. Contact the administrator.";
+      break;
+    case UserData.KEY_EXISTS:
+      s = "This user name exists already. Please choose another.";
+      break;
+    case UserData.KEYS_DONTMATCH:
+      s = "Usernames don't match.";
+      break;
+    case UserData.SECRETS_DONTMATCH:
+      s = "Passwords don't match.";
+      break;
+    
+    }
+    return s;
+  }
+  
+  /**
+   * possible errors
+   * 
+   * TODO - switch to internation version later
+   * 
+   * @param error
+   * @return
+   */
+  public static String getError(int error) {
+    
+    String s = "";
+    switch (error) {
+    case SYSTEM_ERROR:
+      s = "System/Server error.";
+      break;
+    case KEY_EXISTS:
+      s = "Username exists. Try another.";
+      break;
+    case KEYS_DONTMATCH:
+      s = "Usernames do not match.";
+      break;
+    case SECRETS_DONTMATCH:
+      s = "Passwords do not match.";
+      break;
+    case BOTH_DONTMATCH:
+      s = "Your usernames and passwords do not match.";
+      break;
+    case KEYS_SHORT:
+      s = "Add more characters to your usernames.";
+      break;
+    case KEY1_SHORT:
+      s = "Add more characters to your username.";
+      break;
+    case KEY2_SHORT:
+      s = "Add more characters to your username.";
+      break;
+    case SECRETS_SHORT:
+      s = "Add more characters to your passwords.";
+      break;
+    case SECRET1_SHORT:
+      s = "Add more characters to your password.";
+      break;
+    case SECRET2_SHORT:
+      s = "Add more characters to your password.";
+      break;
+    case BOTH_SHORT:
+      s = "Add more characters to both the username and password.";
+      break;
+    case ACCEPT_TERMS:
+      s = "You need to accept the terms of use and privacy agreements to use this application.";
+      break;
+    }
+    
+    return s;
+  }
+  
+  
+  public String getError() {
+    return getError(this.error);
+  }
+}
---
+++ Core/src/com/gawkat/core/server/RpcServiceImpl.java
@@ -0,0 +1,168 @@
+package com.gawkat.core.server;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.gawkat.core.client.account.UserData;
+import com.gawkat.core.client.oauth.OAuthTokenData;
+import com.gawkat.core.client.rpc.RpcService;
+import com.gawkat.core.server.db.User;
+import com.gawkat.core.server.db.oauth.OAuthServer;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+@SuppressWarnings("serial")
+public class RpcServiceImpl extends RemoteServiceServlet implements RpcService {
+
+	/**
+	 * clear a eclipse notification with this
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * get request url
+	 * 
+	 * @return
+	 */
+	private String getRequestUrl() {
+
+		HttpServletRequest request = getThreadLocalRequest();
+		String host = request.getRemoteHost();
+		String path = request.getPathInfo();
+		int port = request.getRemotePort();
+		
+		String url = "";
+		if (port == 80) {
+			url = "http://" + host + path;
+		} else if (port == 443) {
+			url = "https://" + host + path;
+		} else {
+			url = "http://" + host + ":" + port + path;
+		}
+		
+		return url;
+	}
+	
+	/**
+	 * get request url path for oauth - minus port
+	 * 
+	 * ajax requests come in on different ports, as far as I can tell
+	 * maybe its only in hosted mode
+	 * 
+	 * @return
+	 */
+	private String getRequestUrlOAuth() {
+
+		HttpServletRequest request = getThreadLocalRequest();
+		String host = getHost(request);
+		String path = request.getRequestURI();
+		
+		// take off the servlet context path
+		String newPath = "";
+		if (path != null) {
+	    String re = "(.*/)";
+	    Pattern p = Pattern.compile(re);
+	    Matcher m = p.matcher(path);
+	    boolean found = m.find();
+	    if (found == true) {
+	      newPath = m.group(1);
+	    }
+		}
+
+		String url = host + newPath;
+		
+		return url;		
+	}
+	
+	private String getHost(HttpServletRequest request) {
+	  String s = "";
+	  
+	  StringBuffer url = request.getRequestURL();
+	  
+	  int sep = 0;
+	  int col = 0;
+	  for (int i=0; i < url.length(); i++) {
+	    
+	    String c = Character.toString(url.charAt(i));
+	    
+	    if (c.equals(":")) {
+	      col++;
+	      if (col == 2) {
+	        break;
+	      }
+	    }
+	    
+	    if (c.equals("/")) {
+	      sep++;
+	      if (sep == 3) {
+	        break;
+	      }
+	    }
+	    
+	    s += c;
+	  }
+	  
+	  return s;
+	}
+	
+	/**
+	 * test method for rpc
+	 */
+	public String testMethod(String s) {
+		s += " was modified on the server.";
+		return s;
+	}
+
+	/**
+	 * A. ->(B.?) grant request token?
+	 */
+	public OAuthTokenData requestToken(OAuthTokenData tokenData) {
+	  
+	  System.out.println("1. Impl: requestToken: ");
+		
+	  String url = getRequestUrlOAuth();
+		
+	  System.out.println("2. Impl: requestToken: ");
+	  
+		OAuthServer oauth = new OAuthServer();
+		
+		System.out.println("3. Impl: requestToken: ");
+		
+		OAuthTokenData rtnToken = oauth.requestToken(tokenData, url);
+		
+		System.out.println("4. Impl: requestToken: ");
+		
+		return rtnToken;
+	}
+
+  public UserData createUser(UserData userData) {
+    
+    // used to sign the new user
+    String url = getRequestUrlOAuth();
+    
+    User db = new User();
+    return db.createUser(userData, url);
+  }
+  
+  public UserData isUserNameExist(UserData userData) {
+    User db = new User();
+    return db.isUserNameExist(userData);
+  }
+  
+  public UserData forgotPassword(UserData userData) {
+    User db = new User();
+    return db.forgotPassword(userData);
+  }
+  
+  public OAuthTokenData getUserAccessToken(OAuthTokenData appAccessToken) {
+    
+    // used to sign the new user
+    String url = getRequestUrlOAuth();
+    
+    OAuthServer oauth = new OAuthServer();
+    OAuthTokenData rtnToken = oauth.getUserAccessToken(appAccessToken, url);
+    return rtnToken;
+  }
+	
+}
