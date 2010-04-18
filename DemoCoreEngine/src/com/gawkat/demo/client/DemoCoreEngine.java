a41714ed2c69f7bf0e8ffdd0f5139b8e3462065d
branflake2267 <branflake2267@4795bbb2-1543-0410-831c-f9a674249a0d>
2010-04-18 05:37:18 +0000

    had a problem with the same package naming as core, switching to different package naming.

---
+++ DemoCoreEngine/src/com/gawkat/demo/client/DemoCoreEngine.java
@@ -0,0 +1,16 @@
+package com.gawkat.demo.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class DemoCoreEngine implements EntryPoint {
+
+
+  public void onModuleLoad() {
+	 
+	  
+  }
+	
+}
---
+++ DemoCoreEngine/src/com/gawkat/demo/client/RpcServiceAsync.java
@@ -0,0 +1,9 @@
+package com.gawkat.demo.client;
+
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface RpcServiceAsync {
+	
+}
