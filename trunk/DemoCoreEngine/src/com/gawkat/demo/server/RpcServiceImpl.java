9327d56418532939e65c21166776b96981ba6b8d
branflake2267 <branflake2267@4795bbb2-1543-0410-831c-f9a674249a0d>
2010-04-18 05:37:18 +0000

    had a problem with the same package naming as core, switching to different package naming.

---
+++ trunk/DemoCoreEngine/src/com/gawkat/demo/server/RpcServiceImpl.java
@@ -0,0 +1,13 @@
+package com.gawkat.demo.server;
+
+import com.gawkat.demo.client.RpcService;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class RpcServiceImpl extends RemoteServiceServlet implements RpcService {
+
+
+}
