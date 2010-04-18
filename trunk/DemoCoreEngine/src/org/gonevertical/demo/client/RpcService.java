88be6c8b913c4ac550adeac6ae523335ae717978
branflake2267 <branflake2267@4795bbb2-1543-0410-831c-f9a674249a0d>
2010-04-18 03:57:54 +0000

    first commit, adding project to include core

---
+++ trunk/DemoCoreEngine/src/org/gonevertical/demo/client/RpcService.java
@@ -0,0 +1,12 @@
+package org.gonevertical.demo.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+/**
+ * The client side stub for the RPC service.
+ */
+@RemoteServiceRelativePath("rpcService")
+public interface RpcService extends RemoteService {
+	
+}
---
+++ trunk/DemoCoreEngine/src/org/gonevertical/demo/client/RpcServiceAsync.java
@@ -0,0 +1,11 @@
+package org.gonevertical.demo.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface RpcServiceAsync {
+	
+	
+}
