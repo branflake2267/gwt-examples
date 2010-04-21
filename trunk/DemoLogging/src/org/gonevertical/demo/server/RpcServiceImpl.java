fd1cb27be0f351f1a1eebf7a220b4cefd390266b
branflake2267 <branflake2267@4795bbb2-1543-0410-831c-f9a674249a0d>
2010-04-21 06:56:59 +0000

    adding more logging examples stuff

--- trunk/DemoLogging/src/org/gonevertical/demo/server/RpcServiceImpl.java
+++ trunk/DemoLogging/src/org/gonevertical/demo/server/RpcServiceImpl.java
@@ -1,9 +1,17 @@
 package org.gonevertical.demo.server;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
 import java.util.EmptyStackException;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.Priority;
 import org.gonevertical.demo.client.CallData;
 import org.gonevertical.demo.client.RpcService;
 
@@ -16,31 +24,77 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 public class RpcServiceImpl extends RemoteServiceServlet implements RpcService {
 
 	private Logger logger = Logger.getLogger(RpcServiceImpl.class);
-	
+
 	public CallData callServer(CallData callData) {
-		
+
 		if (callData.type == 1) {
 			callData.note = "from server";
 			logger.info("test to server");
-			
+
 		} else if (callData.type == 2) {
-			
-  		testMethod();
-  		
+
+			testMethod();
+
+		} else if (callData.type == 10) {
+
+			callData.note = getLog();
 		}
-		
-		
+
+
 		return callData;
 	}
 
-	private void testMethod() {
-	  
-		try {
-	    throw new EmptyStackException();
-    } catch (Exception e) {
-	    logger.warn("Exception Thrown", e);
+	private String getLog() {
+		File executionlocation = null;
+    try {
+      executionlocation = new File(RpcServiceImpl.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
     }
-	  
-  }
+    String execPath = executionlocation.getParent();
+    
+    execPath = execPath.replaceAll("/WEB-INF", "");
+    
+    String path = execPath + "/logging.log";  
+		
+    File file = new File(path);
+    
+		String r = "";
+		
+		FileInputStream fis = null;
+		BufferedInputStream bis = null;
+		DataInputStream dis = null;
+		BufferedReader br = null;
+		try {
+			fis = new FileInputStream(file);
+			bis = new BufferedInputStream(fis);
+			dis = new DataInputStream(bis);
+			br = new BufferedReader(new InputStreamReader(dis));
+			String s = null;
+			while ((s = br.readLine()) != null) {
+				r += s + "\n";
+			}
+			br.close();
+			fis.close();
+			bis.close();
+			dis.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return r;
+	}
+
+	private void testMethod() {
+
+		try {
+			throw new EmptyStackException();
+		} catch (Exception e) {
+			logger.warn("Exception Thrown", e);
+		}
+
+	}
 
 }
