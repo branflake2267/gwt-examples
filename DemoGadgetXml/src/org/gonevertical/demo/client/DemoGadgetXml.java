45b184dff267a3dd5de9d314b64964d193713a20
branflake2267 <branflake2267@4795bbb2-1543-0410-831c-f9a674249a0d>
2010-04-24 19:12:00 +0000

    demo gadget xml first import

---
+++ DemoGadgetXml/src/org/gonevertical/demo/client/DemoGadgetXml.java
@@ -0,0 +1,14 @@
+package org.gonevertical.demo.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class DemoGadgetXml implements EntryPoint {
+
+  public void onModuleLoad() {
+
+  }
+	
+}
