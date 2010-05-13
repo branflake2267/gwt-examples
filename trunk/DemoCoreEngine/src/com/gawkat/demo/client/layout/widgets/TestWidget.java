0e8223339a730b7a79137e01187eb848988da006
branflake2267 <branflake2267@4795bbb2-1543-0410-831c-f9a674249a0d>
2010-05-13 06:46:53 +0000

    fixed up the login events

--- trunk/DemoCoreEngine/src/com/gawkat/demo/client/layout/widgets/TestWidget.java
+++ trunk/DemoCoreEngine/src/com/gawkat/demo/client/layout/widgets/TestWidget.java
@@ -3,7 +3,10 @@ package com.gawkat.demo.client.layout.widgets;
 import org.gonevertical.core.client.ClientPersistence;
 import org.gonevertical.core.client.account.ui.LoginUi;
 import org.gonevertical.core.client.account.ui.LoginWidget;
+import org.gonevertical.core.client.global.EventManager;
 
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -20,17 +23,38 @@ public class TestWidget extends Composite {
 		
 		initWidget(pWidget);
 		
+		// observe login events
+		cp.addChangeHandler(new ChangeHandler() {
+			public void onChange(ChangeEvent event) {
+				ClientPersistence wcp = (ClientPersistence) event.getSource();
+				if (wcp.getChangeEvent() == EventManager.LOGGEDIN) {
+					drawWidget();
+				} else if (wcp.getChangeEvent() == EventManager.LOGGEDOUT) {
+					pWidget.clear();
+					drawLogin();
+				}
+			}				
+		});	
 	}
 	
 	public void draw() {
 		
 		if (cp.getLoginStatus() == false) {
-			LoginWidget wLogin = LoginWidget.getWidgetStandAlone(cp, LoginUi.LOGIN_VERTICAL);
-			pWidget.add(wLogin);
+			drawLogin();
 			return;
 		}
 		
-		
+		drawWidget();
 	}
 
+	private void drawLogin() {
+		LoginWidget wLogin = LoginWidget.getWidgetStandAlone(cp, LoginUi.LOGIN_VERTICAL);
+		pWidget.add(wLogin);
+	}
+	
+	private void drawWidget() {
+		pWidget.clear();
+		pWidget.add(new HTML("Good Job! You have successfully logged in."));
+  }
+	
 }
