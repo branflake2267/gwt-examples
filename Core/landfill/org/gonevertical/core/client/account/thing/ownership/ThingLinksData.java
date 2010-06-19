924025814d1eaea8238b0938ea8c096f065aeca0
branflake2267 <branflake2267@4795bbb2-1543-0410-831c-f9a674249a0d>
2010-06-19 23:44:39 +0000

    project layout change

--- Core/landfill/org/gonevertical/core/client/account/thing/ownership/ThingLinksData.java
+++ Core/landfill/org/gonevertical/core/client/account/thing/ownership/ThingLinksData.java
@@ -1,6 +1,6 @@
 package org.gonevertical.core.client.account.thing.ownership;
 
-import org.gonevertical.core.client.account.thingset.ThingSetOfData;
+import org.gonevertical.core.client.admin.thingset.ThingSetOfData;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
