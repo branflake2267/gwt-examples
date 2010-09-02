1b04fa9e3584987cc6c7fd3cb1b15c04c5cb5d00
branflake2267 <branflake2267@4795bbb2-1543-0410-831c-f9a674249a0d>
2010-09-02 19:57:31 +0000

    sorry, i'm removing this one for now.

--- Core/landfill/org/gonevertical/core/client/account/permission/ThingPermissionsData.java
+++
@@ -1,9 +0,0 @@
-package org.gonevertical.core.client.account.permission;
-
-import com.google.gwt.user.client.rpc.IsSerializable;
-
-public class ThingPermissionsData implements IsSerializable {
-
-  public ThingPermissionData[] permissions = null;
-  
-}
