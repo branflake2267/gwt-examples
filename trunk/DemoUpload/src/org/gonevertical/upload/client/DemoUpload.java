d4dd50091f4336044d5a5758adf90896daffce9f
branflake2267 <branflake2267@4795bbb2-1543-0410-831c-f9a674249a0d>
2010-09-06 16:05:57 +0000

    layout cleanup

--- trunk/DemoUpload/src/org/gonevertical/upload/client/DemoUpload.java
+++ trunk/DemoUpload/src/org/gonevertical/upload/client/DemoUpload.java
@@ -16,15 +16,19 @@ public class DemoUpload implements EntryPoint {
   	
   	Blobs blobs = new Blobs();
   	
+  	HTML h = new HTML("<a href=\"/mapreduce/status\">See the Map Reduce Demo</a> - only run bible.csv, in root of this project. (actual mapping has been disabled online)");
   	
   	VerticalPanel vp = new VerticalPanel();
   	vp.setWidth("100%");
   	vp.add(fuw);
   	vp.add(new HTML("&nbsp;"));
+  	vp.add(h);
+  	vp.add(new HTML("&nbsp;"));
   	vp.add(blobs);
   	
   	vp.setCellHorizontalAlignment(fuw, VerticalPanel.ALIGN_CENTER);
   	vp.setCellHorizontalAlignment(blobs, VerticalPanel.ALIGN_CENTER);
+  	vp.setCellHorizontalAlignment(h, VerticalPanel.ALIGN_CENTER);
   	
   	RootPanel.get("content").add(vp);
 	  
