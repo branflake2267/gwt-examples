0d85a67b36f899c6b8c7d6c88591fa35289f0815
branflake2267 <branflake2267@4795bbb2-1543-0410-831c-f9a674249a0d>
2010-12-18 16:10:28 +0000

    simple maps demo

---
+++ DemoGWTMaps/src/org/gonevertical/demogwtmaps/server/RpcServiceImpl.java
@@ -0,0 +1,14 @@
+package org.gonevertical.demogwtmaps.server;
+
+import org.gonevertical.demogwtmaps.client.RpcService;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class RpcServiceImpl extends RemoteServiceServlet implements RpcService {
+
+ 
+}
